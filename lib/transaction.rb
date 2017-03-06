a04c93f33fc779d618993bbd2313f283c5527805
Sam Mckay <sammckay12@gmail.com>
2017-03-06 15:11:31 +0000

    created Transaction class with attributes for log

---
+++ lib/transaction.rb
@@ -0,0 +1,11 @@
+class Transaction
+
+  attr_reader :date, :amount, :state
+
+  def initialize(amount:, state:)
+    @date = DateTime.now.strftime('%d/%m/%Y')
+    @amount = amount
+    @state = state
+  end
+
+end
---
+++ spec/transaction_spec.rb
@@ -0,0 +1,28 @@
+require 'transaction'
+
+describe Transaction do
+  let(:transaction) { described_class.new(amount: 10, state: 'credit') }
+
+  describe '#transaction' do
+    context 'upon initialization' do
+      it 'initialises with transaction amount' do
+        expect(transaction.amount).to eq 10
+      end
+      it 'initialises with state' do
+        expect(transaction.state).to eq 'credit'
+      end
+      it 'initialises with date stamp' do
+        expect(transaction.date).to eq DateTime.now.strftime('%d/%m/%Y')
+      end
+    end
+
+  end
+
+
+
+
+
+
+
+
+end
